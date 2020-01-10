3244e7b936efc0d4a1fa2c9e79b49938619f8173
Anshul Agrawal <anshul.jkt@gmail.com>
2020-01-10 13:19:24 -0500

    Cleaned up code and added warning if no API Key supplied.

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const fetch = require('node-fetch');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log();
 var API_KEY = 'DEMO_KEY';
-if(process.env.API_KEY != undefined && process.env.API_KEY != '') {
+if (process.env.API_KEY != undefined && process.env.API_KEY != '') {
   API_KEY = process.env.API_KEY;
+} else {
+  console.log("No API Key specified. This program will be affected by NASA's Demo Key rate limits.");
 }
 
 const API = `https://api.nasa.gov`
@@ -26,7 +28,7 @@ const simpleGet = {
   method: 'GET',
   headers: {
     "Accept": 'application/json'
-  } 
+  }
 }
 
 app.get('/', function (req, res) {
@@ -36,89 +38,89 @@ app.get('/', function (req, res) {
   url = `${API}/planetary/apod?api_key=${API_KEY}`;
   // console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    potdURL = resp.url;
-    altText = resp.explanation;
-    if(potdURL === undefined) {
-      potdURL = "https://apod.nasa.gov/apod/image/1912/J0030_NICER_1024.jpg";
-    }
-    // console.log(`potdURL: ${potdURL}`);
-    res.render('index', {
-      imgSrc: potdURL,
-      text: altText
-    });
-  })
-  .catch(err=> {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      potdURL = resp.url;
+      altText = resp.explanation;
+      if (potdURL === undefined) {
+        potdURL = "https://apod.nasa.gov/apod/image/1912/J0030_NICER_1024.jpg";
+      }
+      // console.log(`potdURL: ${potdURL}`);
+      res.render('index', {
+        imgSrc: potdURL,
+        text: altText
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
-app.get('/asteroids', function(req, res) {
+app.get('/asteroids', function (req, res) {
   //Returns Asteroids using NASA's NeoWS API.
   url = `${API}/neo/rest/v1/feed?detailed=false&api_key=${API_KEY}`;
   console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    res.send(resp);
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
-app.get('/imagery/:lat/:long', function(req, res) {
+app.get('/imagery/:lat/:long', function (req, res) {
   //Returns JSON data linking to an image from NASA's Earth Imagery API
   url = `${API}/planetary/earth/imagery/?lat=${req.params.lat}&lon=${req.params.long}&api_key=${API_KEY}`;
   console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    res.send(resp);
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
-app.get('/marsImagery/:sols', function(req, res) {
+app.get('/marsImagery/:sols', function (req, res) {
   url = `${API}/mars-photos/api/v1/rovers/curiosity/photos?sol=${req.params.sols}&api_key=${API_KEY}`;
   console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    res.send(resp);
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
-app.get('/search/:query', function(req, res) {
+app.get('/search/:query', function (req, res) {
   url = `https://images-api.nasa.gov/search?q=${req.params.query}`
   console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    res.send(resp);
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
-app.get('/epicImagery', function(req, res) {
+app.get('/epicImagery', function (req, res) {
   url = `${API}/EPIC/api/natural/images?api_key=${API_KEY}`
   console.log(url);
   fetch(url, simpleGet)
-  .then(resp => resp.json())
-  .then(resp => {
-    res.send(resp);
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .then(resp => resp.json())
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 })
 
 app.listen(9001, function () {
